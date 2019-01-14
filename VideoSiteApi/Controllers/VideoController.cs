d81ccfb89b381e5fd742b0c386c30773ec4785e0
christianmarziangithub <cmarzian91@gmail.com>
2019-01-14 19:56:02 +0100

    added videocontroller

---
+++ VideoSiteApi/Controllers/VideoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VideoSiteApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoController : ControllerBase
+    {
+        // GET: api/Video
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Video/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+
+
+
+
+
+            return "value";
+        }
+
+        // POST: api/Video
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Video/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
